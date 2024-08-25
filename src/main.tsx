f88e1750913b7acc749e2b0b235a479fb5673400
Michael <mikegoescoding@gmail.com>
2024-08-25 17:05:55 -0500

    housekeeping before styling

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import "bootstrap/dist/js/bootstrap.bundle.min"; // Import Bootstrap JavaScript
-import "./App.css";
+import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
